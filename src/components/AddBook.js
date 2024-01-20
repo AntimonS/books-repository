859fbb17be1f5e4ed1d7aa8779b83b4c3b7b9e94
Ante Debelić <antedebelic@yahoo.com>
2024-01-20 16:23:04 +0100

    added some changes to books-repository

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -68,10 +68,3 @@ function AddBook() {
 }
 
 export default AddBook;
-/*
-<div class="input-group">
-  <span class="input-group-text">First and last name</span>
-  <input type="text" aria-label="First name" class="form-control">
-  <input type="text" aria-label="Last name" class="form-control">
-</div>
-*/
